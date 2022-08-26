c3859bce37a4bfe07a04ba36b95d2e8a1991bdcd
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2022-08-25 20:24:02 -0400

    Time: 115 ms (9.67%), Space: 14.4 MB (97.11%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -3,9 +3,7 @@ class Solution:
         
         dic = {}
         
-        if len(s) != len(t):
-            return False
-        
+
         for letter in s:
             if letter in dic:
                 dic[letter] += 1
@@ -17,5 +15,9 @@ class Solution:
             if letter in dic.keys() and dic[letter] >= 1:
                 dic[letter] -= 1
             else:
+                return False
+            
+        for val in dic.values():
+            if val != 0:
                 return False
         return True
