d4dc377137f02573df69d02319c0d13308557cb9
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2022-09-11 16:22:33 -0400

    Time: 219 ms (12.10%), Space: 13.9 MB (82.78%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -2,38 +2,26 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
         
-        def checkUnique(nums):
-    
-            s = set()
-            for num in nums:
-                if num == '.':
-                    continue
-                if num in s:
-                    return False
-                s.add(num)
-            return True
-
-        for line in board:
-            # If checkUnique returns False
-            if not checkUnique(line):
-                return False
-
-        # check all verticals 
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+        
+        
         for i in range(9):
-            vert = [board[x][i] for x in range(9)]
-            if not checkUnique(vert):
-                return False    
-
-        # Check boxes 3x3
-        for i in range(0,9,3):
-            for j in range(0,9,3):
-                box = [board[i][j+x] for x in range(3)]
-                box2 = [board[i+1][j+x] for x in range(3)]
-                box3 = [board[i+2][j+x] for x in range(3)]
-                res = box + box2 + box3
-                if not checkUnique(res):
+            for j in range(9):
+                if board[i][j] == '.':
+                    continue
+                    
+                if (board[i][j] in rows[i] or 
+                    board[i][j] in cols[j] or
+                    board[i][j] in squares[(i // 3, j // 3)]
+                   ):
+                    print(i,j)
                     return False
-
+                else:
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    squares[(i // 3, j // 3)].add(board[i][j])
         return True
         
     
