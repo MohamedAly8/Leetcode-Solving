fd3ebc20276b185d4703e687fb777cfebee6153f
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2022-09-24 17:55:11 -0400

    Time: 39 ms (76.11%), Space: 13.8 MB (57.20%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,7 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        
+        
+        if n <= 3:
+            return n
 
-        # [_, _, _, _, _, _]
         dp = [1 for i in range(n+1)]
 
         for i in range(n-2,-1,-1):
