aeaa49f3a04d01b9f7c4ada77d9bf733c9982220
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-05-24 21:19:22 -0400

    Time: 48 ms (26.90%) | Memory: 16.3 MB (32.48%) - LeetSync

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -3,24 +3,10 @@ class Solution:
 
 
         r = [0,0]
-        
-        r1 = 0
-        r2 = 0
-
-
         for num in nums:
-
-            temp = r2
-
-            r2 = max(num+r1, r2)
-            r1 = temp 
-
-            # r.append(max(temp, r[-1]))
-            print(r1,r2)
-
-        # print(r)
-
-        return r2
+            temp = num + r[-2]
+            r.append(max(temp, r[-1]))
+        return r[-1]
 
 
             
