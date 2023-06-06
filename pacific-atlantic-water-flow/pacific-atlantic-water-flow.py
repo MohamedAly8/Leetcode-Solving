ba4e86a88c77b89924b19dc0f1d03709a1ea3e5b
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-06-05 23:57:00 -0400

    Time: 285 ms (57.80%) | Memory: 17.9 MB (63.94%) - LeetSync

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -2,50 +2,43 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
 
         ROWS,COLS = len(heights), len(heights[0])
-
         pac, atl = set(), set()
+        res = []
 
         def dfs(r,c,visit, prevHeight):
-            # if already visited or out of bounds
+            # if already visited or out of bounds OR IF HEIGHT IS LESS THAT PREVIOUS HEIGHT
             if ((r,c) in visit or 
                 r < 0 or c < 0 or r == ROWS or c == COLS or 
-                heights[r][c] < prevHeight):
+                not(heights[r][c] >= prevHeight)):
                 return
             
+            # add to visited
             visit.add((r,c))
 
+            # DFS all around 
             dfs(r+1,c,visit,heights[r][c])
             dfs(r-1,c,visit,heights[r][c])
             dfs(r,c+1,visit,heights[r][c])
             dfs(r,c-1,visit,heights[r][c])
 
-
-
         for c in range(COLS):
-
             # first & last row
             dfs(0,c,pac, heights[0][c])
             dfs(ROWS-1,c,atl, heights[ROWS-1][c])
 
-
         for r in range(ROWS):
-
+            # first & last column
             dfs(r, 0, pac, heights[r][0])
             dfs(r, COLS-1, atl, heights[r][COLS-1])
         
-        res = []
-        # brute force every position 
+        # check every cell if it is in both Pac & Atl
         for r in range(ROWS):
             for c in range(COLS):
                 if (r,c) in pac and (r,c) in atl:
                     res.append([r,c])
     
+
         return res
-            
-
-
-            
-
 
 
 
