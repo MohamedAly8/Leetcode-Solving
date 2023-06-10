6c536fe35882da6e5f63911cf5b9101ac1416933
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-06-10 16:51:21 -0400

    Time: 46 ms (36.04%) | Memory: 16.3 MB (40.41%) - LeetSync

--- determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.py
+++ determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.py
@@ -6,10 +6,7 @@ class Solution:
         diff_c = ord(coordinates[0]) - ord('a')
         n = int(coordinates[1]) - 1
 
-        if (n % 2 == 0 and diff_c % 2 == 0) or (n % 2 != 0 and diff_c % 2 != 0):
-            return False
-        
-        return True
+        return not ((n % 2 == 0 and diff_c % 2 == 0) or (n % 2 != 0 and diff_c % 2 != 0))
         
 
 
