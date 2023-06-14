d349784deacb82e6aa423343eddf4bf29ccf3efc
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-06-14 13:06:30 -0400

    Time: 53 ms (25.79%) | Memory: 16.2 MB (66.62%) - LeetSync

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -4,27 +4,18 @@ class Solution:
         dp = { len(s) : 1 }
 
 
-        def dfs(i):
-
-            # already been cached or last position
-            if i in dp:
-                return dp[i]
+        for i in range(len(s)-1, -1,-1):
             
             # no way to decode
             if s[i] == "0":
-                return 0
-            
-            # now it is 1-9, can be taken as single digit
-            res = dfs(i+1)
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
 
             # there is 2nd character and the 2 numbers are 10-26
             if (i + 1 < len(s) and ((s[i] == "1") or (s[i] == "2" and s[i+1] in "0123456"))):
-                res += dfs(i+2)
+                dp[i] += dp[i+2]
             
-            dp[i] = res
-
-            return res
-        
-        return dfs(0)
+        return dp[0]
 
 
