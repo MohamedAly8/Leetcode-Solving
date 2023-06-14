dd4bbe8d0e2669b87dbe541070980565677bc004
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-06-14 13:10:44 -0400

    Time: 45 ms (62.75%) | Memory: 16.4 MB (42.70%) - LeetSync

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -5,16 +5,16 @@ class Solution:
 
 
         for i in range(len(s)-1, -1,-1):
-            
+             
             # no way to decode
             if s[i] == "0":
                 dp[i] = 0
             else:
                 dp[i] = dp[i+1]
 
-            # there is 2nd character and the 2 numbers are 10-26
-            if (i + 1 < len(s) and ((s[i] == "1") or (s[i] == "2" and s[i+1] in "0123456"))):
-                dp[i] += dp[i+2]
+                # there is 2nd character and the 2 numbers are 10-26
+                if (i + 1 < len(s) and ((s[i] == "1") or (s[i] == "2" and s[i+1] in "0123456"))):
+                    dp[i] += dp[i+2]
             
         return dp[0]
 
