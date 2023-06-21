8e3dc6869faae139ae336a20aac002ccbb1dd5da
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-06-21 13:03:27 -0400

    Time: 76 ms (64.37%) | Memory: 16.4 MB (77.83%) - LeetSync

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,24 +4,20 @@ class Solution:
        
         # Creating a set to store the last positions of occurrence
         seen = set()
-        maximum_length = 0
-    
-        # starting the initial point of window to index 0
+        res = 0
         start = 0
         
-        # index of end of window
         for end in range(len(s)):
 
             while s[end] in seen:
                 seen.remove(s[start])
                 start += 1
-
-            print(start,end)
             seen.add(s[end])
-
-            # we know it is unique
-            maximum_length = max(end - start + 1, maximum_length)
+            res = max(res, end-start+1)
         
-        return maximum_length
+        return res
+
+
+
             
                
