88e405bb644b1af352a6380b2bce5d22a67c488c
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-06-22 21:24:05 -0400

    Time: 101 ms (5.25%) | Memory: 17.6 MB (7.86%) - LeetSync

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        l = 0
+        r = 1
+
+
+        # heuristic, 
+        # move away when at negative
+
+
+        # sell when the next transaction results in a decrease of profit
+
+        res = 0
+
+        while r < len(prices):
+
+            print(l,r)
+            profit = prices[r] - prices[l]
+
+            # losing money, move on 
+            if prices[r] - prices[l] < 0:
+                l = r
+                r += 1
+            
+            # if increasing one more day makes profit less, sell
+            elif r < len(prices) - 1 and profit > prices[r+1] - prices[l]:
+                res += profit
+                l = r
+                r += 1
+            
+            else:
+                r += 1
+            
+            if profit > 0 and r == len(prices):
+                res += profit
+
+        return res
+
+
+
