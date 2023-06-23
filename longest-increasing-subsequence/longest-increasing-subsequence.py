369c9086e90b403bd31f08c1bdd5d1ff8c2d49a5
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-06-22 22:07:24 -0400

    Time: 5221 ms (5.02%) | Memory: 16.8 MB (32.08%) - LeetSync

---
+++ longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+
+        # base case at last element LIS=1
+
+        # LIS len-1 : 1 , or 1 + LIS[3] ONLY IF nums[len-1] < nums[len]
+
+        dp = [1] * len(nums)  
+    
+        for i in range(len(nums)-1, -1, -1):
+
+            j = i + 1
+            while(j < len(nums)):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], 1+dp[j])
+                j += 1                    
+            
+        return max(dp)
+
+            
