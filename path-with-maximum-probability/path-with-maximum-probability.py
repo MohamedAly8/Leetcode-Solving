aa9627297d48ece7e39648ea3baa1d65419551cb
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-06-27 22:35:24 -0400

    Time: 743 ms (39.10%) | Memory: 28.1 MB (78.43%) - LeetSync

---
+++ path-with-maximum-probability/path-with-maximum-probability.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+
+        graph = defaultdict(list)
+        queue = deque([start])
+
+
+        for i, (a,b) in enumerate(edges):
+
+            graph[a].append([b,i])
+            graph[b].append([a,i])
+
+        
+        prob = [0.0] * n
+        prob[start] = 1.0
+
+        while queue:
+
+            curr = queue.popleft()
+
+            for neigh, i in graph.get(curr,[]):
+
+                if prob[curr] * succProb[i] > prob[neigh]:
+                    prob[neigh] = prob[curr] * succProb[i]
+                    queue.append(neigh)
+            
+        return prob[end]
+
