152ad065a8f520625b65dc13c42548bc1bfc4765
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-06-27 23:06:41 -0400

    Time: 63 ms (6.68%) | Memory: 16.3 MB (48.88%) - LeetSync

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -19,12 +19,12 @@ class Solution:
                     list2 = list2.next
                 cur = cur.next
             
-            while list1:
+            if list1:
                 cur.next = list1
                 cur = cur.next
                 list1 = list1.next
             
-            while list2:
+            if list2:
                 cur.next = list2
                 cur = cur.next
                 list2 = list2.next
