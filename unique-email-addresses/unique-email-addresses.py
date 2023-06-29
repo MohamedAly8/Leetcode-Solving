439882bf876b6e3ac11400d6a83685bdc4e8c122
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-06-28 23:44:21 -0400

    Time: 73 ms (46.08%) | Memory: 16.5 MB (51.30%) - LeetSync

---
+++ unique-email-addresses/unique-email-addresses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+
+        res = set()
+
+        for email in emails:
+
+            email, domain = email.split("@")
+            email = email.split("+")[0]
+                    
+            
+            tmp = ""
+            for c in email:
+                if c == ".":
+                    continue
+                tmp += c
+            res.add((tmp, domain))
+
+        return len(res)
+
+
