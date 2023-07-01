d16875f2c68674c8b12b8ed8de105f5663d44603
Mohamed Aly <69180195+MohamedAly8@users.noreply.github.com>
2023-07-01 00:07:52 -0400

    Time: 71 ms (90.65%) | Memory: 16.9 MB (23.04%) - LeetSync

--- check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
@@ -5,20 +5,19 @@ class Solution:
         x1, y1 = coordinates[0]
         x2, y2 = coordinates[1]
 
-        if (x2-x1) != 0:
-            m = (y2-y1) / (x2-x1)
-        else:
-            m = -1
-        b = y2 - (m*x2)
-        for x,y in coordinates[2:]:
+        # vertical line
+        if x1 == x2:
+            for x,y in coordinates:
 
-            # vertical line
-            if m == -1:
                 if x != x1:
                     return False
-            elif y != (x*m+b):
-                print("here")
+            return True
+
+        
+        m = (y2-y1) / (x2-x1)
+        b = y2 - m*x2
+
+        for x,y in coordinates[2:]:
+            if y != (m*x+b):
                 return False
-
         return True
-
